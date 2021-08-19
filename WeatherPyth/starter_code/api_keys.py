77dd34e12f0d189aa9f6a3062ad3f3ca8f497fb0
Curtis Anderson <canderson5786@gmail.com>
2021-08-19 15:40:01 -0500

    Project complete

--- WeatherPyth/starter_code/api_keys.py
+++ WeatherPyth/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6ee795699cf1b10acdbefeccce4c941b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCpVlOKblrC0dXHqMDA7BqOQFKvUkbYaiU"
